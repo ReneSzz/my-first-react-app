c45eb95f4beca5714e1dd7528cbbce3f771743a5
ReneSzz <renesuar99@gmail.com>
2023-08-11 17:48:50 -0400

    added resume section

---
+++ my-first-react-app/src/components/resume.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Person from './components/personDetails'
+import Education from './components/education'
+import Experience from './components/experience'
